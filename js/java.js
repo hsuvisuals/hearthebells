889fc4ccad3c5060d4d9029fc0ffc85737417eb3
Jonathan Hsu <jonathanchun.hsu@sjsu.edu>
2018-11-01 16:32:57 -0700

    uploading site

---
+++ js/java.js
@@ -0,0 +1,112 @@
+    function PlaySound(soundobj) {
+    var thissound=document.getElementById(soundobj);
+    thissound.play();
+}
+
+function StopSound(soundobj) {
+    var thissound=document.getElementById(soundobj);
+    thissound.pause();
+    thissound.currentTime = 0;
+}
+    
+  function fadeOutEffect() {
+    var fadeTarget = document.getElementById("bell1");
+    var fadeEffect = setInterval(function () {
+        if (!fadeTarget.style.opacity) {
+            fadeTarget.style.opacity = 1;
+        }
+        if (fadeTarget.style.opacity < 0.1) {
+            clearInterval(fadeEffect);
+        } else {
+            fadeTarget.style.opacity -= 0.1;
+        }
+    }, 50);
+}
+        
+document.getElementById("bell1").addEventListener('click', fadeOutEffect);
+
+    
+    function fadeOutEffect() {
+    var fadeTarget = document.getElementById("bell3");
+    var fadeEffect = setInterval(function () {
+        if (!fadeTarget.style.opacity) {
+            fadeTarget.style.opacity = 1;
+        }
+        if (fadeTarget.style.opacity < 0.1) {
+            clearInterval(fadeEffect);
+        } else {
+            fadeTarget.style.opacity -= 0.1;
+        }
+    }, 50);
+}
+
+
+document.getElementById("bell3").addEventListener('click', fadeOutEffect);
+
+
+    function fadeOutEffect() {
+    var fadeTarget = document.getElementById("bell4");
+    var fadeEffect = setInterval(function () {
+        if (!fadeTarget.style.opacity) {
+            fadeTarget.style.opacity = 1;
+        }
+        if (fadeTarget.style.opacity < 0.1) {
+            clearInterval(fadeEffect);
+        } else {
+            fadeTarget.style.opacity -= 0.1;
+        }
+    }, 50);
+}
+
+document.getElementById("bell4").addEventListener('click', fadeOutEffect);
+   
+    
+    function fadeOutEffect() {
+    var fadeTarget = document.getElementById("bell5");
+    var fadeEffect = setInterval(function () {
+        if (!fadeTarget.style.opacity) {
+            fadeTarget.style.opacity = 1;
+        }
+        if (fadeTarget.style.opacity < 0.1) {
+            clearInterval(fadeEffect);
+        } else {
+            fadeTarget.style.opacity -= 0.1;
+        }
+    }, 50);
+}
+
+document.getElementById("bell5").addEventListener('click', fadeOutEffect);
+    
+
+    function fadeOutEffect() {
+    var fadeTarget = document.getElementById("bell6");
+    var fadeEffect = setInterval(function () {
+        if (!fadeTarget.style.opacity) {
+            fadeTarget.style.opacity = 1;
+        }
+        if (fadeTarget.style.opacity < 0.1) {
+            clearInterval(fadeEffect);
+        } else {
+            fadeTarget.style.opacity -= 0.1;
+        }
+    }, 50);
+}
+
+document.getElementById("bell6").addEventListener('click', fadeOutEffect);
+   
+    function fadeOutEffect() {
+    var fadeTarget = document.getElementById("bell7");
+    var fadeEffect = setInterval(function () {
+        if (!fadeTarget.style.opacity) {
+            fadeTarget.style.opacity = 1;
+        }
+        if (fadeTarget.style.opacity < 0.1) {
+            clearInterval(fadeEffect);
+        } else {
+            fadeTarget.style.opacity -= 0.1;
+        }
+    }, 50);
+}
+
+document.getElementById("bell7").addEventListener('click', fadeOutEffect);
+    
